4e8cd2fa14b4eb46936fc3159ce3b69d238251cc
Sandeep Kelkar <sandeepkelkar67@gmail.com>
2016-01-09 15:27:45 +0530

    added moment to front end

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+
 
 app.use(express.static(__dirname + '/public'));
 
@@ -12,12 +14,13 @@ io.on('connection', function (socket) {
 	socket.on('message', function (message) {
 		console.log('Message recieved: ' + message.text);
 
-
+		message.timestamp = moment().valueOf()
 		io.emit('message', message);
 	});
 
 	socket.emit('message', {
-		text: 'Welcome to the chat application!'
+		text: 'Welcome to the chat application!',
+		timestamp: moment.valueOf()
 	});
 });
 http.listen(PORT, function () {
